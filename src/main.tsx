912c8946585049c12d1c167438efade46dc9b4c9
TABOUDA <marouane.essaadouni@gmail.com>
2026-03-11 10:04:26 +0000

    TP1

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AuthProvider } from './features/auth/AuthContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+createRoot(document.getElementById('root')!).render(
+<StrictMode>
+<AuthProvider>
+<App />
+</AuthProvider>
+</StrictMode>
+);
